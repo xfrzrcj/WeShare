c82ff5a2f4ab8895f228437f1b4bb323a602d90d
xfrzrcj <xfrzrcj@163.com>
2018-05-16 12:12:14 +0800

    add

---
+++ js/rem.js
@@ -0,0 +1,18 @@
+/*
+ * Created by pxy on 2016/01/04
+ */
+(function (doc,win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            docEl.style.fontSize = 100 * (clientWidth / 540) + 'px';
+            if(clientWidth>=980){
+                docEl.style.fontSize=120 + 'px';
+            }
+        };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
