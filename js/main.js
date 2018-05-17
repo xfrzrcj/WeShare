5d7d2044789c6d26a37fe86bb85e810cd95ac4a6
syy0726 <184654660@qq.com>
2018-05-17 15:44:32 +0800

    点赞

--- js/main.js
+++ js/main.js
@@ -14,10 +14,45 @@ $(function(){
 	$(".sort li").click(function(){
 		$(this).addClass('active').siblings().removeClass('active')
 	})
-	$(".btn-box button").click(function(){
+	/*$(".btn-box button").click(function(){
 		$(this).addClass('active').siblings().removeClass('active')
-	});
+	});*/
 	$(".return-top").click(function() {
-      $("html,body").animate({scrollTop:0}, 500);
-  }); 
+        $("html,body").animate({scrollTop:0}, 500);
+    }); 
+     $(".like").click(function() {
+       
+    });
 })
+function dianzan(th){
+	//点赞
+        var hash=th.parent().parent().index();
+        $.ajax({
+            contentType:"application/json",
+            data:'{"from":"n1EjkdHBDpdjFfVaeJqMQW11RhYqrrjCZR7","to":"n1huG18QbozvJn3Hdyn11sBKkvHQ9pdRz7D","value":"0","nonce":3,"gasPrice":"1000000","gasLimit":"2000000","contract":{"function":"star","args":"['+hash+']"}}',
+            type:'post',
+            url:'https://testnet.nebulas.io/v1/user/call',
+            dataType:'json',
+            success:function(d){
+               var num=parseInt(th.find('span').eq(1).html())+1
+               th.find('span').eq(1).html(num);
+               th.addClass('active').siblings().removeClass('active')
+            }
+        })
+}
+function tucao(th){
+	//点赞
+        var hash=th.parent().parent().index();
+        alert(hash)
+        $.ajax({
+            contentType:"application/json",
+            data:'{"from":"n1EjkdHBDpdjFfVaeJqMQW11RhYqrrjCZR7","to":"n1huG18QbozvJn3Hdyn11sBKkvHQ9pdRz7D","value":"0","nonce":3,"gasPrice":"1000000","gasLimit":"2000000","contract":{"function":"booing","args":"['+hash+']"}}',
+            type:'post',
+            url:'https://testnet.nebulas.io/v1/user/call',
+            dataType:'json',
+            success:function(d){
+               var num=parseInt(th.find('span').eq(1).html())+1
+               th.find('span').eq(1).html(num);
+            }
+        })
+}
